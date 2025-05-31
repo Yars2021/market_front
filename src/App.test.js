83751bc1d29082c4d1cc8db632c8df94a595d13e
Yars2021 <yaroslav.sukhovey@yandex.ru>
2025-05-31 15:21:27 +0300

    + CI

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Component testing", () => {
+  test("App component", () => {
+    const { container } = render(<App/>);
+    const { firstChild} = container;
+    expect(firstChild).toHaveClass("main_page");
+  });
+
+  test("UsersTable component", () => {
+    const { container } = render(<App/>);
+    const { firstChild} = container;
+    const { tableComponent } = firstChild;
+    expect(tableComponent).toHaveAttribute("url", "http://localhost:8080/users");
+  });
 });
