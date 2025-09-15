d43bfae25be6d5fa5ad17d35cebb37766fd7875a
Yars2021 <79992244+Yars2021@users.noreply.github.com>
2025-09-15 17:19:03 +0300

    Update UsersTable.js

--- src/components/UsersTable.js
+++ src/components/UsersTable.js
@@ -29,9 +29,6 @@ function UsersTable(props) {
     return (
         <div className={"table_container"}>
             <div className={"table_container_row"}>
-                Application header
-            </div>
-            <div className={"table_container_row"}>
                 <AddForm
                     url={props.url}
                     onSubmit={() => {setRefresh(!refresh)}}
